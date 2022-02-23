f8abc1f26daaec26c4691e81abd9b152ad28ce22
Daniel Danis <daniel.danis@jax.org>
2022-02-23 18:12:51 +0100

    Add phenopacket schema & protobuf to the project

--- phenopacketlab-restapi/src/main/java/module-info.java
+++ phenopacketlab-restapi/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module phenopacketlab.restapi {
     requires phenopacketlab.autoconfigure;
 
+    requires phenopacket.schema;
+
     requires spring.boot;
     requires spring.boot.autoconfigure;
     requires spring.web;
