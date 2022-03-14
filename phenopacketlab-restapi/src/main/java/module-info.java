f0f2601631bf1969617ed2533864f2a8079b6f6f
Daniel Danis <daniel.danis@jax.org>
2022-03-14 17:18:07 +0100

    Create stub endpoints (`DataController` and `TestController`)

--- phenopacketlab-restapi/src/main/java/module-info.java
+++ phenopacketlab-restapi/src/main/java/module-info.java
@@ -3,9 +3,15 @@ module phenopacketlab.restapi {
 
     requires phenopacket.schema;
 
+    requires org.slf4j;
+
     requires spring.boot;
     requires spring.boot.autoconfigure;
     requires spring.web;
+
+    // TODO - consider removing
+    requires com.google.protobuf;
+    requires com.google.protobuf.util;
 
     opens org.monarchinitiative.phenopacketlab.restapi;
     opens org.monarchinitiative.phenopacketlab.restapi.controller;
