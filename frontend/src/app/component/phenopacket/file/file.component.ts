a4666070a54baf469b23bb565a4618e33fa3771a
Baha El Kassaby <baha.elkassaby@gmail.com>
2022-05-18 16:29:22 -0400

    Fix attribute chips in files

--- frontend/src/app/component/phenopacket/file/file.component.ts
+++ frontend/src/app/component/phenopacket/file/file.component.ts
@@ -123,14 +123,13 @@ export class FileComponent implements OnInit {
     return key + " -> " + value;
   }
   getAttributeKeys(file: File) {
-    let attributesKeys = file.fileAttribute.keys();
     let resultKeys = [];
     // remove description key
-    for (let key in attributesKeys) {
+    file.fileAttribute.forEach((value: string, key: string) => {
       if (key !== 'description') {
         resultKeys.push(key);
       }
-    }
+    });
     return resultKeys;
   }
   getColor(key: string) {
