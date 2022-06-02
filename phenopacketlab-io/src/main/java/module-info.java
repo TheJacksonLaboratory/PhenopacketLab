fcdaf895ccf79491d80905bddc32791e4bf6be94
Daniel Danis <daniel.gordon.danis@protonmail.com>
2022-06-02 11:01:18 -0400

    Update phenopackets schema module name.
    Signed-off-by: Daniel Danis <daniel.gordon.danis@protonmail.com>

--- phenopacketlab-io/src/main/java/module-info.java
+++ phenopacketlab-io/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module phenopacketlab.io {
 
     requires transitive phenopacketlab.model;
-    requires phenopacket.schema;
+    requires org.phenopackets.schema;
     requires org.monarchinitiative.phenol.io;
 
     requires org.slf4j;
