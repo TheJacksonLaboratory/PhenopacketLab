cb8e082e8335b0f6960a29de4eefa8d2568bf90b
Daniel Danis <daniel.gordon.danis@protonmail.com>
2022-06-03 16:42:20 -0400

    Deprecate `OntologyConstantsController`.
    Signed-off-by: Daniel Danis <daniel.gordon.danis@protonmail.com>

--- phenopacketlab-restapi/src/main/java/org/monarchinitiative/phenopacketlab/restapi/controller/OntologyConstantsController.java
+++ phenopacketlab-restapi/src/main/java/org/monarchinitiative/phenopacketlab/restapi/controller/OntologyConstantsController.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 // @CrossOrigin // TODO - do we need this here?
+@Deprecated(forRemoval = true)
 @RestController
 @RequestMapping(value = "${api.version}/ontology")
 public class OntologyConstantsController {
