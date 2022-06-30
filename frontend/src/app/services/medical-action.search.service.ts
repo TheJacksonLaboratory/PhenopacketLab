7787fb9f7f2e2d5167932ef0f7de7f49bbacf1b0
Baha El Kassaby <baha.elkassaby@gmail.com>
2022-06-30 17:16:10 -0400

    Add bodySites fake endpoint
    *Add fake endpoint
    *Add bodySite ontology for human as id-names json file
    *Add corrresponding environment URLs
    *Add medicalAction service

---
+++ frontend/src/app/services/medical-action.search.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable, Subject } from "rxjs";
+import { environment } from "src/environments/environment";
+import { Phenopacket } from "../models/phenopacket";
+import { BaseSearchService } from "./base-search.service";
+
+const bodySitesUrl = environment.PHENOPACKETLAB_BODY_SITE_URL;
+
+@Injectable({ providedIn: 'root' })
+export class MedicalActionService extends BaseSearchService {
+    selectedSearchItems: any;
+    selectedSearchItemSubject: BehaviorSubject<any>;
+    
+    constructor(private http: HttpClient) {
+        super(http);
+        this.selectedSearchItems = {}
+        this.selectedSearchItemSubject = new BehaviorSubject(this.selectedSearchItems)
+    }
+
+    getSelectedSearchItems() {
+        return this.selectedSearchItems;
+    }
+
+    setSelectedSearchItems(searchItems: any) {
+        this.selectedSearchItems = searchItems;
+        this.selectedSearchItemSubject.next(searchItems);
+    }
+
+    getAll(): Observable<any> {
+        return this.getAllBodySites();
+    }
+
+    public getAllBodySites(): Observable<any> {
+        return this.http.get(bodySitesUrl);
+    }
+
+    // public queryBodySiteById(id: string): Observable<any> {
+    //     return this.http.get(`${bodySitesUrl}/${id}`);
+    // }
+}
