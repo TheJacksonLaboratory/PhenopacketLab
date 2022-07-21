04a4ea576f05b6d353f8717b12c09c23282a0b5c
Baha El Kassaby <baha.elkassaby@gmail.com>
2022-07-21 16:49:17 -0400

    update model

--- frontend/src/app/models/medical-action.ts
+++ frontend/src/app/models/medical-action.ts
@@ -1,15 +1,35 @@
-import { ExternalReference, OntologyClass, Procedure, TimeElement, TimeInterval } from "./base";
+import { Convert, ExternalReference, OntologyClass, Procedure, TimeElement, TimeInterval } from "./base";
 import { Quantity } from "./measurement";
 
 
-export class MedicalAction {
+export class MedicalAction extends Convert {
     // can be Procedure, Treatment, RadiationTherapy or TherapeuticRegimen
     action: any;
     treatmentTarget: OntologyClass;
     treatmentIntent: OntologyClass;
     responseToTreatment: OntologyClass;
-    adverseEvents: OntologyClass;
+    adverseEvents: OntologyClass[];
     treatmentTerminationReason: OntologyClass;
+
+    static create(obj: any): MedicalAction {
+        const medicalAction = new MedicalAction();
+        // action
+        let action = obj['action'];
+        if (action['code']) {
+            medicalAction.action = Procedure.convert(action);
+        } else if (action['agent']) {
+            medicalAction.action = Treatment.convert(action);
+        } else if (action['modality']) {
+            medicalAction.action = RadiationTherapy.convert(action);
+        } else if (action['identifier']) {
+            medicalAction.action = TherapeuticRegimen.convert(action);
+        }
+        medicalAction.treatmentTarget = OntologyClass.convert(obj['treatmentTarget']);
+        medicalAction.treatmentIntent = OntologyClass.convert(obj['treatmentIntent']);
+        medicalAction.adverseEvents = OntologyClass.convert(obj['adversEvents']);
+        medicalAction.treatmentTerminationReason = OntologyClass.convert(obj['treatmentTerminationReason']);
+        return medicalAction;
+    }
 }
 
 export class Treatment {
@@ -22,11 +42,29 @@ export class Treatment {
     toString() {
         return "Treatment";
     }
+
+    static convert(obj: any): Treatment {
+        const treatment = new Treatment();
+        treatment.agent = OntologyClass.convert(obj['agent']);
+        treatment.routeOfAdministration = OntologyClass.convert(obj['routeOfAdministration']);
+        treatment.doseIntervals = DoseInterval.convert(obj['doseIntervals']);
+        treatment.drugType = obj['drugType'];
+        treatment.cumulativeDose = Quantity.convert(obj['cumulativeDose']);
+        return treatment;
+    } 
 }
-export class DoseInterval {
+export class DoseInterval extends Convert {
     quantity: Quantity;
     scheduleFrequency: OntologyClass;
     interval: TimeInterval;
+
+    static create(obj: any): DoseInterval {
+        const doseInterval = new DoseInterval();
+        doseInterval.quantity = Quantity.convert(obj['quantity']);
+        doseInterval.scheduleFrequency = OntologyClass.convert(obj['scheduleFrequency']);
+        doseInterval.interval = TimeInterval.convert(obj['interval']);
+        return doseInterval;
+    }
 }
 export enum DrugType {
     UNKNOWN_DRUG_TYPE = "Unknown",
@@ -43,6 +81,15 @@ export class RadiationTherapy {
     toString() {
         return "Radiation therapy";
     }
+
+    static convert(obj: any): RadiationTherapy {
+        const radiationTherapy = new RadiationTherapy();
+        radiationTherapy.modality = OntologyClass.convert(obj['modality']);
+        radiationTherapy.bodySite = OntologyClass.convert(obj['bodySite']);
+        radiationTherapy.dosage = obj['dosage'];
+        radiationTherapy.fractions = obj['fractions'];
+        return radiationTherapy;
+    }
 }
 
 export enum RegimenStatus {
@@ -55,9 +102,23 @@ export class TherapeuticRegimen {
     identifier: OntologyClass | ExternalReference;
     startTime: TimeElement;
     endTime: TimeElement;
-    regimenStatus: RegimenStatus;
+    status: RegimenStatus;
 
     toString() {
         return "Therapeutic regimen";
+    }
+
+    static convert(obj: any): TherapeuticRegimen {
+        const therapeuticRegimen = new TherapeuticRegimen();
+        let identifier = obj['identifier'];
+        if (identifier['label']) {
+            therapeuticRegimen.identifier = OntologyClass.convert(identifier);
+        } else {
+            therapeuticRegimen.identifier = ExternalReference.convert(identifier);
+        }
+        therapeuticRegimen.startTime = TimeElement.convert(obj['startTime']);
+        therapeuticRegimen.endTime = TimeElement.convert(obj['endTime']);
+        therapeuticRegimen.status = obj['status'];
+        return therapeuticRegimen;
     }
 }
