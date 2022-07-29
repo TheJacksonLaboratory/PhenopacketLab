dea4e60e0f6db767b4f98ec7ac0c0b1316fd086d
Baha El Kassaby <baha.elkassaby@gmail.com>
2022-07-29 13:53:57 -0400

    Fix build error

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -2,8 +2,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { CohortListComponent } from './component/cohort-list/cohort-list.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { FamilyListComponent } from './component/family-list/family-list.component';
-import { SelectableOntologyTreeComponent } from './component/ontology-tree/selectable/selectable-ontology-tree.component';
-import { SimpleOntologyTreeComponent } from './component/ontology-tree/simple/simple-ontology-tree.component';
 import { PhenopacketComponent } from './component/phenopacket/phenopacket.component';
 
 
@@ -12,8 +10,6 @@ const routes: Routes = [
   { path: 'families', component: FamilyListComponent },
   { path: 'cohorts', component: CohortListComponent },
   { path: 'phenopackets', component: PhenopacketComponent },
-  { path: 'simple-ontology-tree', component: SimpleOntologyTreeComponent },
-  { path: 'selectable-ontology-tree', component: SelectableOntologyTreeComponent }
 ];
 
 export const routing = RouterModule.forRoot(routes);
