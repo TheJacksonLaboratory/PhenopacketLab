564b490914dc333286eb6dbcc930047465625c3f
belkassaby <baha.elkassaby@gmail.com>
2022-10-22 10:23:36 -0400

    Update phenopacket creator to use primeng stepper

--- frontend/src/app/component/pheno-creator/pheno-creator.component.ts
+++ frontend/src/app/component/pheno-creator/pheno-creator.component.ts
@@ -1,96 +1,78 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { OntologyClass, TimeElement } from 'src/app/models/base';
-import { KaryotypicSex, Sex, Status } from 'src/app/models/individual';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MessageService } from 'primeng/api';
+import { Subscription } from 'rxjs';
+
 import { Phenopacket } from 'src/app/models/phenopacket';
+import { PhenopacketService } from 'src/app/services/phenopacket.service';
 
 @Component({
   selector: 'app-pheno-creator',
   templateUrl: './pheno-creator.component.html',
-  styleUrls: ['./pheno-creator.component.scss']
-
+  styleUrls: ['./pheno-creator.component.scss'],
+  providers: [MessageService]
 })
-
-export class PhenoCreatorComponent implements OnInit {
+export class PhenoCreatorComponent implements OnInit, OnDestroy {
 
   phenopacket: Phenopacket;
 
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
-  thirdFormGroup: FormGroup;
-  fourthFormGroup: FormGroup;
-  fifthFormGroup: FormGroup;
-  sixthFormGroup: FormGroup;
-  seventhFormGroup: FormGroup;
-  eigthFormGroup: FormGroup;
+  // primeng stepper
+  items: MenuItem[];
+  subscription: Subscription;
 
-  constructor(private _formBuilder: FormBuilder) {
+  constructor(private messageService: MessageService, public phenopacketService: PhenopacketService) {
   }
 
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      phenopacketIdCtrl: ['', Validators.required],
-      summaryCtrl: '',
-      dobCtrl: new FormControl(new Date()),
-      toleCtrl: [''],
-      sexCtrl: new FormControl(''),
-      karyoSexCtrl: new FormControl(''),
-      genderCtrl: new FormControl(new OntologyClass('', '')),
-      vitalStatusFormGroup: new FormGroup({
-        statusCtrl: new FormControl(Status.UNKNOWN_STATUS),
-        timeOfDeathCtrl: new FormControl(new TimeElement()),
-        causeOfDeathCtrl: new FormControl(new OntologyClass('', '')),
-        survivalTimeCtrl: new FormControl()
-      })
+    this.items = [{
+      label: 'Individual',
+      routerLink: 'individual'
+    },
+    {
+      label: 'Phenotypic Feature(s)',
+      routerLink: 'phenotypic-features'
+    },
+    {
+      label: 'Measurement(s)',
+      routerLink: 'measurements'
+    },
+    {
+      label: 'Biosample(s)',
+      routerLink: 'biosamples'
+    },
+    {
+      label: 'Interpretation(s)',
+      routerLink: 'interpretations'
+    },
+    {
+      label: 'Disease(s)',
+      routerLink: 'diseases'
+    },
+    {
+      label: 'Medical Action(s)',
+      routerLink: 'medical-actions'
+    },
+    {
+      label: 'File(s)',
+      routerLink: 'files'
+    },
+    {
+      label: 'Validate',
+      routerLink: 'validate'
+    }
+    ];
 
-    });
-    // this.firstFormGroup.get('viatlStatusFormGroup').get('causeOfDeathCtrl')
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: '',
-    });
-    this.thirdFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required],
-    });
-    this.fourthFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required],
-    });
-    this.fifthFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required],
-    });
-    this.sixthFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required],
-    });
-    this.seventhFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required],
-    });
-    this.eigthFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required],
+    this.subscription = this.phenopacketService.validated$.subscribe((phenopacket) => {
+      this.messageService.add({ severity: 'success', summary: 'Phenopacket created',
+        detail: 'The phenopacket with the ID ' + phenopacket.subject.id + ' has been successfully validated.' });
     });
 
-  }
-
-  getSexes() {
-    // tslint:disable-next-line:radix
-    return Object.values(Sex).filter(x => !(parseInt(x) >= 0));
-  }
-
-  getKaryotypicSexes() {
-    // tslint:disable-next-line:radix
-    return Object.values(KaryotypicSex).filter(x => !(parseInt(x) >= 0));
-  }
-  getStatuses() {
-    // tslint:disable-next-line:radix
-    return Object.values(Status).filter(x => !(parseInt(x) >= 0));
   }
 
-  getCauseOfDeath(): OntologyClass {
-    return OntologyClass.create(this.fifthFormGroup.get('vitalStatusFormGroup').get('causeOfDeathCtrl'));
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
-  changeCauseOfDeath(eventObj: OntologyClass) {
-    // this.procedureCode = eventObj;
-    // // update medicalAction
-    // if (this.medicalAction) {
-    //   this.medicalAction.action.code = this.procedureCode;
-    // }
-  }
+
 }
