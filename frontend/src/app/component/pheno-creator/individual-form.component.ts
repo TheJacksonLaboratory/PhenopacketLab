bf2795caf97333581723c259404d8f76fb175dc0
belkassaby <baha.elkassaby@gmail.com>
2022-12-01 15:44:26 -0500

    Individual form in stepper UI update
    *update individual fields

--- frontend/src/app/component/pheno-creator/individual-form.component.ts
+++ frontend/src/app/component/pheno-creator/individual-form.component.ts
@@ -3,9 +3,10 @@ import { Router } from '@angular/router';
 import { TreeNode } from 'primeng/api';
 import { Subscription } from 'rxjs';
 import { OntologyClass, TimeElement } from 'src/app/models/base';
-import { Gender, Individual, KaryotypicSex, Sex, Status, VitalStatus } from 'src/app/models/individual';
+import { Gender, Individual, KaryotypicSex, Sex, Status } from 'src/app/models/individual';
 import { Phenopacket } from 'src/app/models/phenopacket';
 import { PhenopacketService } from 'src/app/services/phenopacket.service';
+import {CohortService} from '../../services/cohort.service';
 
 @Component({
     selector: 'app-individual-form',
@@ -19,26 +20,24 @@ export class IndividualFormComponent implements OnInit, OnDestroy {
     phenopacketSubscription: Subscription;
     submitted = false;
 
-    id: string;
     summary: string;
-
+    ageType: any;
     timeOfLastEncounter: TimeElement;
-    sex: any;
-    karyotypicSex: any;
-    gender: any;
+    selectedSex: any;
+    selectedKaryotypicSex: any;
+    selectedGender: any;
     dob: Date;
     individual: Individual;
 
-    status: string;
+    selectedStatus: any;
     timeOfDeath: TimeElement;
-    causeOfDeath: string;
     survivalTime: number;
 
     selectedCauseOfDeath: OntologyClass;
     causeOfDeaths: any[];
     causeOfDeathSubscription: Subscription;
 
-    constructor(public phenopacketService: PhenopacketService, private router: Router) {
+    constructor(public phenopacketService: PhenopacketService, private cohortService: CohortService, private router: Router) {
 
     }
     ngOnDestroy(): void {
@@ -54,7 +53,6 @@ export class IndividualFormComponent implements OnInit, OnDestroy {
         this.phenopacket = this.phenopacketService.phenopacket;
         this.causeOfDeathSubscription = this.phenopacketService.getMondoDiseases().subscribe(nodes => {
             this.causeOfDeaths = <TreeNode[]>nodes.data;
-            console.log(this.causeOfDeaths);
 
         });
         if (this.phenopacket === undefined) {
@@ -63,72 +61,60 @@ export class IndividualFormComponent implements OnInit, OnDestroy {
         }
         // set up
         if (this.phenopacket) {
-            this.id = this.phenopacket.id;
-            this.sex = this.phenopacket.subject?.sex;
-            this.karyotypicSex = this.phenopacket.subject?.karyotypicSex;
-            this.gender = this.phenopacket.subject?.gender;
+            this.selectedSex = this.phenopacket.subject?.sex;
+            this.selectedKaryotypicSex = this.phenopacket.subject?.karyotypicSex;
+            this.selectedGender = this.phenopacket.subject?.gender;
             this.timeOfLastEncounter = this.phenopacket.subject?.timeAtLastEncounter;
-            this.status = this.phenopacket.subject?.vitalStatus?.status;
+            this.selectedStatus = this.phenopacket.subject?.vitalStatus?.status;
             this.timeOfDeath = this.phenopacket.subject?.vitalStatus?.timeOfDeath;
-            this.causeOfDeath = this.phenopacket.subject?.vitalStatus?.causeOfDeath?.toString();
+            this.selectedCauseOfDeath = this.phenopacket.subject?.vitalStatus?.causeOfDeath;
             this.survivalTime = this.phenopacket.subject?.vitalStatus?.survivalTimeInDays;
 
         }
 
     }
 
-    nextPage() {
-        console.log('next page');
-        console.log(this.phenopacket);
-        if (this.phenopacket.id) {
-            // this.phenopacketService.setPhenopacket(this.phenopacket);
-            this.phenopacketService.phenopacket = this.phenopacket;
-            // vital status
-
-            // cause of death
-            if (this.selectedCauseOfDeath) {
-                if (this.phenopacket.subject.vitalStatus) {
-                    this.phenopacket.subject.vitalStatus = new VitalStatus();
-                    this.phenopacket.subject.vitalStatus.causeOfDeath = this.selectedCauseOfDeath;
-                    console.log(this.selectedCauseOfDeath);
-                }
-            }
-            this.router.navigate(['pheno-creator/phenotypic-features']);
-            console.log(this.phenopacketService.getPhenopacket());
-
-            return;
-        }
-        this.submitted = true;
-
-    }
-
     /**
      *
-     * @param timeOfLastEncouter a TimeElement
+     * @param timeOfLastEncounter a TimeElement
      */
-    updateTimeOfLastEncounter(timeOfLastEncouter: any) {
-        console.log(timeOfLastEncouter);
-        this.phenopacket.subject.timeAtLastEncounter = timeOfLastEncouter;
-        this.phenopacketService.setPhenopacket(this.phenopacket);
+    updateTimeOfLastEncounter(timeOfLastEncounter: any) {
+        this.timeOfLastEncounter = timeOfLastEncounter;
+        this.phenopacket.subject.timeAtLastEncounter = this.timeOfLastEncounter;
     }
 
+    updateSex(event) {
+        this.selectedSex = event.value;
+        // update phenopacket
+        this.phenopacket.subject.sex = this.selectedSex;
+    }
+    updateKaryotypicSex(event) {
+        this.selectedKaryotypicSex = event.value;
+        this.phenopacket.subject.karyotypicSex = this.selectedKaryotypicSex;
+    }
+    updateGender(event) {
+        this.selectedGender = event.value;
+        this.phenopacket.subject.gender = this.selectedGender;
+    }
+    updateStatus(event) {
+        this.selectedStatus = event.value;
+        this.phenopacket.subject.vitalStatus.status = this.selectedStatus;
+    }
+    updateSurvivalTime(event) {
+        this.survivalTime = event.value;
+        this.phenopacket.subject.vitalStatus.survivalTimeInDays = this.survivalTime;
+    }
     /**
      *
      * @param timeOfDeath a TimeElement
      */
     updateTimeOfDeath(timeOfDeath: any) {
-        if (this.phenopacket.subject.vitalStatus === undefined) {
-            this.phenopacket.subject.vitalStatus = new VitalStatus();
-        }
-        this.phenopacket.subject.vitalStatus.timeOfDeath = timeOfDeath;
-        this.phenopacketService.setPhenopacket(this.phenopacket);
+        this.timeOfDeath = timeOfDeath;
+        this.phenopacket.subject.vitalStatus.timeOfDeath = this.timeOfDeath;
     }
-    updateCauseOfDeath(causeOfDeath: OntologyClass) {
-        if (this.phenopacket.subject.vitalStatus === undefined) {
-            this.phenopacket.subject.vitalStatus = new VitalStatus();
-        }
-        this.phenopacket.subject.vitalStatus.causeOfDeath = causeOfDeath;
-        this.phenopacketService.setPhenopacket(this.phenopacket);
+    updateCauseOfDeath(event) {
+        this.selectedCauseOfDeath = new OntologyClass(event.node.key, event.node.label);
+        this.phenopacket.subject.vitalStatus.causeOfDeath = this.selectedCauseOfDeath;
     }
     getSexes() {
         // tslint:disable-next-line:radix
@@ -148,22 +134,14 @@ export class IndividualFormComponent implements OnInit, OnDestroy {
         return Object.values(Status).filter(x => !(parseInt(x) >= 0));
     }
 
-    getCauseOfDeath() {
+    nextPage() {
+        if (this.phenopacket.id) {
+            // TODO Check if id already exists
+            this.phenopacketService.phenopacket = this.phenopacket;
+            this.router.navigate(['pheno-creator/phenotypic-features']);
+            return;
+        }
+        this.submitted = true;
 
     }
-    changeCauseOfDeath(eventObj: OntologyClass) {
-        // this.procedureCode = eventObj;
-        // // update medicalAction
-        // if (this.medicalAction) {
-        //   this.medicalAction.action.code = this.procedureCode;
-        // }
-    }
-    editStatus() {
-        // TODO
-    }
-    editTimeOfLastEncounter() {
-        // TODO
-    }
-
-
 }
