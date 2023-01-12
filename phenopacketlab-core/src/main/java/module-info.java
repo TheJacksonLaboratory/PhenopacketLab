4a7f79de44cf63d1b936ddfffa4e2ccce3df1e70
belkassaby <baha.elkassaby@gmail.com>
2023-01-12 15:30:24 -0500

    Make miner package visible to autoconfigure module

--- phenopacketlab-core/src/main/java/module-info.java
+++ phenopacketlab-core/src/main/java/module-info.java
@@ -12,7 +12,7 @@ module phenopacketlab.core {
     exports org.monarchinitiative.phenopacketlab.core.ontology;
     exports org.monarchinitiative.phenopacketlab.core.subtree to phenopacketlab.restapi;
     opens org.monarchinitiative.phenopacketlab.core.subtree to com.fasterxml.jackson.databind;
-    exports org.monarchinitiative.phenopacketlab.core.miner to phenopacketlab.restapi;
+    exports org.monarchinitiative.phenopacketlab.core.miner to phenopacketlab.restapi, phenopacketlab.autoconfigure;
     opens org.monarchinitiative.phenopacketlab.core.miner to com.fasterxml.jackson.databind;
 
 }
