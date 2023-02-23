b37280f6bb37fae56ad63e86dc03b1c67fab98d1
iimpulse <Michael.Gargano@jax.org>
2023-02-23 15:23:08 -0700

    adding faq examples

--- frontend/src/app/component/help/help.component.ts
+++ frontend/src/app/component/help/help.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Contact } from '../../models/contact';
 @Component({
     selector: 'app-help',
     templateUrl: './help.component.html',
     styleUrls: ['./help.component.scss']
 })
 export class HelpComponent implements OnInit {
+
     constructor() {
     }
 
