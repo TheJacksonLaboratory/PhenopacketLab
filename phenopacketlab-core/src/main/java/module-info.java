672516411a1fa2fb98953fbd8f34966ed1f1a3d5
Daniel Danis <daniel.gordon.danis@protonmail.com>
2023-03-08 12:31:31 -0500

    Remove unused dependencies from `core` POM, rearrange module-info files
    Signed-off-by: Daniel Danis <daniel.gordon.danis@protonmail.com>

--- phenopacketlab-core/src/main/java/module-info.java
+++ phenopacketlab-core/src/main/java/module-info.java
@@ -8,13 +8,13 @@ module phenopacketlab.core {
 
     exports org.monarchinitiative.phenopacketlab.core;
     exports org.monarchinitiative.phenopacketlab.core.disease;
-    exports org.monarchinitiative.phenopacketlab.core.ontology;
     exports org.monarchinitiative.phenopacketlab.core.functionalannotation;
-    exports org.monarchinitiative.phenopacketlab.core.subtree to phenopacketlab.restapi;
-    opens org.monarchinitiative.phenopacketlab.core.subtree to com.fasterxml.jackson.databind;
     exports org.monarchinitiative.phenopacketlab.core.miner to phenopacketlab.restapi, phenopacketlab.autoconfigure;
-
     exports org.monarchinitiative.phenopacketlab.core.model;
     exports org.monarchinitiative.phenopacketlab.core.model.util;
+    exports org.monarchinitiative.phenopacketlab.core.ontology;
+    exports org.monarchinitiative.phenopacketlab.core.subtree to phenopacketlab.restapi;
+
     opens org.monarchinitiative.phenopacketlab.core.model to com.fasterxml.jackson.databind;
+    opens org.monarchinitiative.phenopacketlab.core.subtree to com.fasterxml.jackson.databind;
 }
