24c0882cbbb4967850c9baf9f419fae9d4e27c4e
belkassaby <baha.elkassaby@gmail.com>
2023-04-26 15:54:34 -0400

    Migrate measurement to primeng #77 #94

--- phenopacketlab-frontend/src/app/component/phenopacket/measurement/measurement.component.ts
+++ phenopacketlab-frontend/src/app/component/phenopacket/measurement/measurement.component.ts
@@ -1,11 +1,13 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { animate, state, style, transition, trigger } from '@angular/animations';
-import { MatDialog } from '@angular/material/dialog';
 
-import { MessageDialogComponent } from '../../shared/message-dialog/message-dialog.component';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { ConfirmationService, MessageService } from 'primeng/api';
+
 import { Measurement } from 'src/app/models/measurement';
 import { MeasurementDetailDialogComponent } from './measurement-detail/measurement-detail-dialog/measurement-detail-dialog.component';
-import { DataPresentMatTableDataSource } from '../../shared/DataPresentMatTableDataSource';
+import { Phenopacket } from 'src/app/models/phenopacket';
+import { Utils } from '../../shared/utils';
 
 @Component({
     selector: 'app-measurement',
@@ -20,133 +22,81 @@ import { DataPresentMatTableDataSource } from '../../shared/DataPresentMatTableD
         ]),
     ],
 })
-export class MeasurementComponent implements AfterViewInit, OnInit {
+export class MeasurementComponent implements OnInit {
 
     @Input()
     measurements: Measurement[];
-
+    @Input()
+    phenopacket: Phenopacket;
     @Output() onMeasurementsChanged = new EventEmitter<Measurement[]>();
 
-    // Table items
-    displayedColumns = ['assay', 'value', 'procedure', 'time', 'remove'];
+    showTable = false;
 
-    measurementDataSource = new DataPresentMatTableDataSource<Measurement>();
-    measurementCount: number;
+    ref: DynamicDialogRef;
 
-    // searchparams
-    currSearchParams: any = {};
-
-    expandedElement: Measurement | null;
-
-    dialogRef: any;
-    spinnerDialogRef: any;
-
-    constructor(public dialog: MatDialog) {
+    constructor(public dialogService: DialogService,
+                private messageService: MessageService,
+                private confirmationService: ConfirmationService) {
     }
 
     ngOnInit() {
-        if (this.measurements === undefined) {
-            this.measurements = [];
+        if (this.measurements && this.measurements.length > 0) {
+            this.showTable = true;
         }
-        this.measurementDataSource.data = this.measurements;
-        this.onMeasurementsChanged.emit(this.measurements);
-
-    }
-
-    ngAfterViewInit() {
-
     }
 
     /**
      * Add a new measurement with default values or no values
      */
     addMeasurement(measurement?: Measurement) {
-        if (measurement === undefined) {
-            // Add through a dialog to choose from type of Actions
-            const measurementDetailData = { 'title': 'Edit measurement' };
-            measurementDetailData['displayCancelButton'] = true;
-            const dialogRef = this.dialog.open(MeasurementDetailDialogComponent, {
-                width: '1000px',
-                data: measurementDetailData
-            });
-            dialogRef.afterClosed().subscribe(result => {
-                if (result !== undefined) {
-                    const updatedMeasurement = result.measurement;
-                    if (updatedMeasurement) {
-                        // update measurement
-                        this.measurements.push(updatedMeasurement);
-                        this.measurementDataSource.data = this.measurements;
-                        // emit change
-                        this.onMeasurementsChanged.emit(this.measurements);
-                    }
+        if (measurement === undefined || measurement === null) {
+            measurement = new Measurement();
+        }
+        this.ref = this.dialogService.open(MeasurementDetailDialogComponent, {
+            header: 'Edit Measurement',
+            width: '70%',
+            contentStyle: { 'overflow': 'auto' },
+            baseZIndex: 10000,
+            resizable: true,
+            draggable: true,
+            data: { measurement: measurement,
+                    phenopacket: this.phenopacket }
+        });
+
+        this.ref.onClose.subscribe((measure: Measurement) => {
+            if (measure) {
+                const indexToUpdate = this.measurements.findIndex(item => item.assay.id === measure.assay.id);
+                if (indexToUpdate === -1) {
+                    measure.key = Utils.getBiggestKey(this.measurements) + 1;
+                    this.measurements.push(measure);
+                } else {
+                    this.measurements[indexToUpdate] = measure;
+                    this.measurements = Object.assign([], this.measurements);
                 }
-            });
-            return dialogRef;
-        } else {
-            this.measurements.push(measurement);
-        }
-        this.measurementDataSource.data = this.measurements;
-        this.onMeasurementsChanged.emit(this.measurements);
-
-        // TODO push changes to api
-    }
-
-    /**
-     * Removes the chosen element, if ok is pressed on the popup window.
-     * @param element
-     * @returns
-     */
-    deleteMeasurement(element: Measurement) {
-        const msgData = { 'title': 'Delete Measurement' };
-        msgData['description'] = `Delete the Measurement with the ID "${this.getId(element)}" ?`;
-        msgData['displayCancelButton'] = true;
-        const dialogRef = this.dialog.open(MessageDialogComponent, {
-            width: '400px',
-            data: msgData
-        });
-        dialogRef.afterClosed().subscribe(result => {
-            if (result) {
-                this.removeFromDatasource(element);
+                this.showTable = true;
+                // emit change
+                this.onMeasurementsChanged.emit(this.measurements);
             }
         });
-        return dialogRef;
     }
 
-    removeFromDatasource(measurement: Measurement) {
-        this.measurements.forEach((element, index) => {
-            if (element === measurement) {
-                this.measurements.splice(index, 1);
+    deleteMeasurement(measurement: Measurement) {
+        this.confirmationService.confirm({
+          message: 'Are you sure you want to delete \'' + measurement.assay?.id + '\'?',
+          header: 'Confirm',
+          icon: 'pi pi-exclamation-triangle',
+          accept: () => {
+            this.measurements = this.measurements.filter(val => val.key !== measurement.key);
+            if (this.measurements.length === 0) {
+              this.showTable = false;
             }
+            this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Measurement Deleted', life: 3000 });
+          },
+          reject: () => {
+            this.confirmationService.close();
+          }
         });
-        this.measurementDataSource.data = this.measurements;
-        this.onMeasurementsChanged.emit(this.measurements);
-
-    }
-
-    /**
-     * Retrieve the correct measurement id
-     * @param measurement
-     * @returns id
-     */
-    getId(measurement: Measurement) {
-        if (measurement) {
-            return measurement.assay.id;
-        }
-        return '';
-    }
-
-    /**
-     * Retrieve the assay
-     * @param measurement
-     * @returns assay
-     */
-    getAssay(measurement: Measurement) {
-        const assay = measurement.assay;
-        if (assay) {
-            return assay.toString();
-        }
-        return '';
-    }
+      }
 
     getValue(measurement: Measurement) {
         let measurementValue;
@@ -158,27 +108,6 @@ export class MeasurementComponent implements AfterViewInit, OnInit {
             return '';
         }
         return measurementValue.toString();
-    }
-
-    getProcedure(measurement: Measurement) {
-        const procedure = measurement.procedure;
-        if (procedure) {
-            return procedure.toString();
-        }
-        return '';
-    }
-
-    getTimeOfMeasurement(measurement: Measurement) {
-        const tom = measurement.timeObserved;
-        if (tom) {
-            return tom.toString();
-        }
-        return '';
-    }
-
-    expandCollapse(element: any) {
-        this.expandedElement = this.expandedElement === element ? null : element;
-
     }
 
 }
