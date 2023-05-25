fcb97e0d482e03b2a57324b65993df74e7c73241
Daniel Danis <daniel.gordon.danis@protonmail.com>
2023-05-25 13:25:54 -0400

    Fix resource data.
    Signed-off-by: Daniel Danis <daniel.gordon.danis@protonmail.com>

--- phenopacketlab-restapi/src/test/java/org/monarchinitiative/phenopacketlab/restapi/controller/MetadataControllerTest.java
+++ phenopacketlab-restapi/src/test/java/org/monarchinitiative/phenopacketlab/restapi/controller/MetadataControllerTest.java
@@ -92,74 +92,74 @@ public class MetadataControllerTest {
             case "HP" -> {
                 return Resource.of("hp",
                         "Human Phenotype Ontology",
+                        "http://purl.obolibrary.org/obo/hp.json",
                         "2022-12-15",
                         "HP",
-                        "http://purl.obolibrary.org/obo/HP_",
-                        "http://purl.obolibrary.org/obo/hp.json");
+                        "http://purl.obolibrary.org/obo/HP_");
             }
             case "EFO" -> {
                 return Resource.of("efo",
                         "Experimental Factor Ontology",
+                        "http://www.ebi.ac.uk/efo/efo.owl",
                         "3.49.0",
                         "EFO",
-                        "http://www.ebi.ac.uk/efo/EFO_",
-                        "http://www.ebi.ac.uk/efo/efo.owl");
+                        "http://www.ebi.ac.uk/efo/EFO_");
             }
             case "GENO" -> {
                 return Resource.of("geno",
                         "Genotype Ontology",
+                        "http://purl.obolibrary.org/obo/geno.json",
                         "2022-08-10",
                         "GENO",
-                        "http://purl.obolibrary.org/obo/GENO_",
-                        "http://purl.obolibrary.org/obo/geno.json");
+                        "http://purl.obolibrary.org/obo/GENO_");
             }
             case "MONDO" -> {
                 return Resource.of("mondo",
                         "MONDO Disease Ontology",
+                        "http://purl.obolibrary.org/obo/mondo.json",
                         "2022-12-01",
                         "MONDO",
-                        "http://purl.obolibrary.org/obo/MONDO_",
-                        "http://purl.obolibrary.org/obo/mondo.json");
+                        "http://purl.obolibrary.org/obo/MONDO_");
             }
             case "SO" -> {
                 return Resource.of("so",
                         "Sequence types and features ontology",
+                        "http://purl.obolibrary.org/obo/uberon.json",
                         "2021-11-22",
                         "SO",
-                        "http://purl.obolibrary.org/obo/UBERON_",
-                        "http://purl.obolibrary.org/obo/uberon.json");
+                        "http://purl.obolibrary.org/obo/UBERON_");
             }
             case "UBERON" -> {
                 return Resource.of("uberon",
                         "Uber-anatomy ontology",
+                        "http://purl.obolibrary.org/obo/hp.json",
                         "2022-12-13",
                         "UBERON",
-                        "http://purl.obolibrary.org/obo/HP_",
-                        "http://purl.obolibrary.org/obo/hp.json");
+                        "http://purl.obolibrary.org/obo/HP_");
             }
             case "HGNC" -> {
                 return Resource.of("hgnc",
                         "HUGO Gene Nomenclature Committee",
+                        "http://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/hgnc_complete_set.txt",
                         "HGNC_VERSION",
                         "HGNC",
-                        "http://identifiers.org/hgnc/HGNC:",
-                        "http://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/hgnc_complete_set.txt");
+                        "http://identifiers.org/hgnc/HGNC:");
             }
             case "NCIT" -> {
                 return Resource.of("ncit",
                         "NCI Thesaurus",
+                        "http://purl.obolibrary.org/obo/ncit.owl",
                         "22.07d",
                         "NCIT",
-                        "http://purl.obolibrary.org/obo/NCIT_",
-                        "http://purl.obolibrary.org/obo/ncit.owl");
+                        "http://purl.obolibrary.org/obo/NCIT_");
             }
             case "GSSO" -> {
                 return Resource.of("gsso",
                         "GSSO - the Gender, Sex, and Sexual Orientation ontology",
+                        "http://purl.obolibrary.org/obo/gsso.owl",
                         "UNKNOWN",
                         "GSSO",
-                        "http://purl.obolibrary.org/obo/GSSO_",
-                        "http://purl.obolibrary.org/obo/gsso.owl");
+                        "http://purl.obolibrary.org/obo/GSSO_");
             }
             default -> {
                 return null;
