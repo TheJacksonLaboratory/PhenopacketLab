720eaee462e404236c8678237c9c83738a4885d9
belkassaby <baha.elkassaby@gmail.com>
2023-07-07 12:18:24 -0400

    migrate biosample table to primeng #123
    *migrate to primeng table
    *use prime ng layout for biosample detail component

--- phenopacketlab-frontend/src/app/component/phenopacket/biosample/biosample-detail/biosample-detail.component.spec.ts
+++ phenopacketlab-frontend/src/app/component/phenopacket/biosample/biosample-detail/biosample-detail.component.spec.ts
@@ -1,13 +1,4 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterTestingModule } from '@angular/router/testing';
 import { PhenopacketModule } from '../../phenopacket.module';
 import { BiosampleDetailComponent } from './biosample-detail.component';
 
@@ -20,16 +11,7 @@ describe('BiosampleDetailComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ BiosampleDetailComponent ],
       imports: [
-        MatDialogModule,
-        NoopAnimationsModule,
-        MatIconModule,
-        MatDividerModule,
-        MatListModule,
         PhenopacketModule,
-        MatButtonModule,
-        MatTooltipModule,
-        RouterTestingModule,
-        HttpClientTestingModule
       ]
     })
     .compileComponents();
--- phenopacketlab-frontend/src/app/component/phenopacket/biosample/biosample-detail/biosample-detail.component.ts
+++ phenopacketlab-frontend/src/app/component/phenopacket/biosample/biosample-detail/biosample-detail.component.ts
@@ -1,26 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
-import { MatDialog } from '@angular/material/dialog';
 import { BioSample } from 'src/app/models/biosample';
 import { Measurement } from 'src/app/models/measurement';
 import { File } from 'src/app/models/base';
 import { PhenotypicFeature } from 'src/app/models/phenotypic-feature';
-import { BiosampleDetailDialogComponent } from './biosample-detail-dialog/biosample-detail-dialog.component';
 
 @Component({
   selector: 'app-biosample-detail',
   templateUrl: './biosample-detail.component.html',
-  styleUrls: ['./biosample-detail.component.scss'],
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed, void', style({ height: '0px', minHeight: '0' })),
-      state('expanded', style({ height: '*' })),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-      transition('expanded <=> void', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ]
+  styleUrls: ['./biosample-detail.component.scss']
 })
-
 export class BiosampleDetailComponent implements OnInit {
 
   @Input()
@@ -28,31 +16,9 @@ export class BiosampleDetailComponent implements OnInit {
 
   id: string;
 
-  constructor(public dialog: MatDialog) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  openEditDialog() {
-    const phenotypicDetailData = { 'title': 'Edit biosample' };
-    phenotypicDetailData['biosample'] = this.biosample;
-    phenotypicDetailData['displayCancelButton'] = true;
-    const dialogRef = this.dialog.open(BiosampleDetailDialogComponent, {
-      width: '750px',
-      data: phenotypicDetailData
-    });
-    dialogRef.afterClosed().subscribe(result => {
-      if (result !== undefined) {
-        const updatedBiosample = result.biosample;
-        if (updatedBiosample !== undefined) {
-          // update feature
-          this.biosample = updatedBiosample;
-          // emit change
-          // this.onFeatureChanged.emit(this.phenotypicFeature);
-        }
-      }
-    });
-    return dialogRef;
   }
 
   getPhenotypicFeatures() {
